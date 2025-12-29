f7f63d86d8422f20e5d38d795d6d86ea5248153b
daylyn-n <daylynnguyen33@gmail.com>
2025-12-29 14:06:13 -0800

    test mouse position

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include "../include/algorithms.hpp"
 #include "../include/grid.hpp"
 #include "../constants/constants.hpp"
@@ -10,6 +11,33 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "MAZE");
     window.setFramerateLimit(60);
-    Grid grid(GRID_WIDTH, GRID_HEIGHT);
-    dfsMazeAnimation(grid, 0,0);
+    // Grid grid(GRID_WIDTH, GRID_HEIGHT);
+    // dfsMazeAnimation(grid, 0,0);
+    
+    sf::Vector2i previousPosition(-1, -1);
+
+    // test for mouse position
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+            {
+                std::cout << "Clicked" << std::endl;
+            }
+            if(event.type == sf::Event::Closed)
+                window.close();
+            
+        }
+         // Update mouse position every frame
+        sf::Vector2i position = sf::Mouse::getPosition(window);
+
+        if(position != previousPosition)
+        {
+            std::cout << "Mouse Position: " << position.x << ", " << position.y << std::endl;
+            previousPosition = position;
+        }
+        
+    }
 }
